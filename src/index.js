b7256df19ecdddd2bd1be37ee691dab6db62a442
heuzin <matheusqs.arq@gmail.com>
2019-07-19 15:10:01 -0400

    Applied applyMiddleware

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+  ),
   /* applyMiddleware goes here */
 );
 
